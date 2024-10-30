928a869a43b8f161d59fc893b5378b95b50559b0
alexkof158 <63129192+alexkof158@users.noreply.github.com>
2024-10-30 14:45:42 -0500

    css

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from '@mui/material';
 
-import './Header.css'; // Importa tu archivo CSS
+import './Header.css';
 import DropdownMenu from '../components/documentaciones/DropdownMenu ';
 
 const Header = () => {
   return (
     <>
-      <br />
+      {/* Layout de noticias */}
+      <div className="layout-news">
+        <h5>Donaciones al Nequi: 3013730306 -- Developer Web: ING Manuel Cogollo</h5>
+      </div>
+      
       <header className="barra">
         <div className="contenedor">
           <div className="contenido-barra">
-            
             <DropdownMenu />
             <Link to="/firma-corp">
               <Button variant="contained" color="primary" size="small">Firma Corporativa</Button>
@@ -23,7 +26,6 @@ const Header = () => {
             <Link to="/feedback">
               <Button variant="contained" color="primary" size="small">Comentarios</Button>
             </Link>
-            
           </div>
         </div>
       </header>
